a5d688007a32a97a396c3a3f2d356d08a1c7222a
BrianEstebanGauna <brianestebangauna@gmail.com>
2023-05-25 16:57:16 -0300

    contacto

---
+++ contacto.js
@@ -0,0 +1,14 @@
+
+
+const $form = document.getElementById('form')
+const $buttonMailto = document.getElementById('correo')
+
+$form.addEventListener('submit', handleSubmit)
+
+function handleSubmit(event) {
+    event.preventDefault()
+    const form = new FormData(this)
+    $buttonMailto.setAttribute('href', `mailto:brianestebangauna@gmai.com?subject=nombre ${form.get('name')}  correo ${form.get('email')}&body=${form.get('message')}`)
+    $buttonMailto.click()
+}
+
